c9a6f1e0a453fd3654365dc7945848ea7a267b4d
Ishan Singh <136164702+ishansingh1010@users.noreply.github.com>
2023-09-02 17:41:22 +0530

    fixingBugs

--- weather_app/routes/weatherRoutes.js
+++ weather_app/routes/weatherRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express')
 
 const weather = require('../controller/weatherController')
+const { userAuthentication } = require('../../middleware/authToken')
 
 const router = express.Router()
 
-router.get('/weatherInfo/:userId', weather.weatherInfo)
-router.post('/addFavoritePlace/:userId', weather.addFavoritePlace)
-router.post('/removeFavoritePlace/:userId', weather.removeFavoritePlace)
-router.get('/listFavoritePlaces/:userId', weather.listFavoritePlaces)
-router.get('/favoritePlacesWeather/:userId', weather.favoritePlacesWeather)
-router.get('/searchWeather', weather.searchWeather)
+router.get('/weatherInfo/:userId', userAuthentication, weather.weatherInfo)
+router.post('/addFavoritePlace/:userId', userAuthentication, weather.addFavoritePlace)
+router.post('/removeFavoritePlace/:userId', userAuthentication, weather.removeFavoritePlace)
+router.get('/listFavoritePlaces/:userId', userAuthentication, weather.listFavoritePlaces)
+router.get('/favoritePlacesWeather/:userId', userAuthentication, weather.favoritePlacesWeather)
+router.get('/searchWeather', userAuthentication, weather.searchWeather)
 
 module.exports = router
